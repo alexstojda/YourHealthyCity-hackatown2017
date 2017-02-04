73ab2081f7aeaf17e5232529a44e139d6170e223
Ilias <ideros21@gmail.com>
2017-02-04 16:18:32 -0500

    return stub data when querying /restaurants

--- api/restaurants.js
+++ api/restaurants.js
@@ -1,7 +1,64 @@
+const geolib = require('geolib')
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-  res.send('list the restaurants here')
+  const restaurants = getRestaurants()
+  res.send(restaurants)
 })
+
+function getRestaurants(){
+  return [
+    {
+      id: 1,
+      name: 'Foo',
+      location: {
+        latitude: 45.5085997,
+        longitude: -73.6207597
+      },
+      meals: [
+        {
+          name: 'steak',
+          price: 17.5,
+          nutrition: {
+            calories: 1150,
+            carbohydrates_grams: 20,
+            proteins_grams: 12,
+            fats_grams: 4
+          }
+        },
+        {
+          name: 'salad',
+          price: 8,
+          nutrition: {
+            calories: 780,
+            carbohydrates_grams: 7,
+            proteins_grams: 0.5,
+            fats_grams: 0.1
+          }
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Bar',
+      location: {
+        latitude: 45.510996,
+        longitude: -73.616470
+      },
+      meals: [
+        {
+          name: 'rice',
+          price: 12,
+          nutrition: {
+            calories: 900,
+            carbohydrates_grams: 25,
+            proteins_grams: 9,
+            fats_grams: 2
+          }
+        }
+      ]
+    }
+  ]
+}
 
 module.exports = router
