a63064b3fbae5f5d024baea9cd4296f37c201c29
Ilias <ideros21@gmail.com>
2017-02-05 08:28:53 -0500

    WIP add "place" to restaurant

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -41,3 +41,15 @@ function createRestaurant(){
       })
   }
 }
+
+(function initializePlacesSearch(){
+  var defaultBounds = new google.maps.LatLngBounds(
+  new google.maps.LatLng(-33.8902, 151.1759),
+  new google.maps.LatLng(-33.8474, 151.2631));
+
+  var input = document.getElementById('gmaps');
+
+  var searchBox = new google.maps.places.SearchBox(input, {
+    bounds: defaultBounds
+  });
+})()
